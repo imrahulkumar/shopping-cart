64d2395abbb4694abb9a567fd64fcc330faba644
Rahul kumar <rahulkumar151995@gmail.com>
2020-05-21 19:13:56 +0530

    changes

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import {data} from '../util/util'
+import { from } from 'rxjs';
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.scss'],
 })
 export class HelpComponent implements OnInit {
-
+  items:any[]= data;
+  @ViewChild(IonInfiniteScroll, {static:false}) infiniteScroll: IonInfiniteScroll;
   constructor() { }
 
   ngOnInit() {}
+
+
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++) {
+        this.items.push( this.items.length );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+      
+      if (data.length == 1000) {
+        infiniteScroll.target.disabled = true;
+      }
+
+    }, 500);
+  }
+
 
 }
